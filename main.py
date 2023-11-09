f519e6d905edfdf71ab64ef57687873d910cc75e
TiaoVenanincio <tiaovenancio2@gmail.com>
2023-11-09 14:25:59 -0300

    atualizando main

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from src import tiraFotos, algoritmoTreinador, deletaFace, reconhece, controlado
 sair = False
 while sair != True:
     print()
-    print("1. Login")
-    print("2. Registro")
-    print("3. Deletar registro")
+    print("1. Reconhecer rosto registrado")
+    print("2. Registrar um novo rosto")
+    print("3. Deletar um registro")
     print("4. Registros existentes")
     print()
     print("5. Sair")
@@ -13,26 +13,20 @@ while sair != True:
 
     opcao = int(input("Insira a opção desejada: "))
 
-    if opcao == 1:  #Login
+    if opcao == 1:  #Reconhecer um rosto registrado
         codigo = int(input("Insira seu código: "))
         reconhece.reconhecimento(codigo)
 
     elif opcao == 2: #Registro de um novo rosto
-        senha = int(input("Insira a senha de adm: "))
-        if senha == 1234:
-            codigo = controlador.gera_codigo()
-            mensagem = "O seu codigo é: " + str(codigo)
-            print(mensagem)
-            tiraFotos.tiraFoto(codigo)
-            algoritmoTreinador.treina_algoritmo(codigo)
-        else:
-            print("Senha incorreta!")
+        codigo = controlador.gera_codigo()
+        mensagem = "O seu codigo é: " + str(codigo)
+        print(mensagem)
+        tiraFotos.tiraFoto(codigo)
+        algoritmoTreinador.treina_algoritmo(codigo)
 
     elif opcao == 3: #Deleta um registro
-        senha = int(input("Insira a senha de adm: "))
-        if senha == 1234:
-            codigo = int(input("Insira o codigo da face que deseja deletar: "))
-            deletaFace.deletarFace(codigo)
+        codigo = int(input("Insira o codigo da face que deseja deletar: "))
+        deletaFace.deletarFace(codigo)
     
     elif opcao == 4: #Imprie os registros existentes
         arquivo = '.\data\codigos.txt'
